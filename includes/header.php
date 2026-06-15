4981ce38822280a9445510d406192a2e474118f5
Metman Karmastorm <metman.karmastorm@gmail.com>
2026-06-15 17:11:21 -0400

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@ $accessNames = [
 ?>
 <header class="site-header">
     <div class="brand">
-        <img src="/assets/img/Logo_3.png" alt="Heisenberg Studios logo" class="brand-logo">
+        <img src="/assets/img/Logo_3.png?v=1" alt="Heisenberg Studios logo" class="brand-logo">
         <span class="brand-text">Heisenberg Studios</span>
     </div>
 
